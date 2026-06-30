862933519ce734ed335fee431672ecc51a018a5c
Bradford Hovinen <bradford@hovinen.me>
2026-06-30 20:46:04 +0200

    Fix test so that it is diabled with no-default-features

--- test-that/tests/result_of_matcher_test.rs
+++ test-that/tests/result_of_matcher_test.rs
@@ -13,6 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#[cfg(feature = "anyhow")]
 use anyhow::anyhow;
 use test_that::prelude::*;
 
